bdd1873b0a484299138a1da3c989e12072cbe1cb
ev <ev@evang.dk>
2014-03-26 11:10:37 +0100

    added project to git

---
+++ LevelReader/GameFrameWork/TiledMap.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using System.Xml;
+using Windows.ApplicationModel;
+using Windows.Data.Xml.Dom;
+using Windows.Storage;
+
+namespace LevelReader.GameFrameWork
+{
+    public class TiledMap
+    {
+
+        #region Variables
+        private Dictionary<String, int[,]> _layers;
+        private String[] _layerNames;
+        private int _width;
+        private int _height;
+        private int _tileWidth;
+        private int _tileHeight;
+        #endregion
+
+        #region Properties
+        public int NumberOfLevels { get {return _layers.Count;} }
+
+        public Dictionary<string, int[,]> Layers { get { return _layers; } }
+
+        public int Width { get { return _width; } }
+
+        public int Height { get { return _height; } } 
+
+        public String[] LayerNames { get { return _layerNames; } }
+
+        public int TileWidth { get { return _tileWidth; } }
+
+        public int TileHeight { get { return _tileHeight; } }
+        #endregion
+
+        #region Constructor
+        public TiledMap(String xmlFile)
+        {
+            try
+            {
+                XmlReader reader = XmlReader.Create(xmlFile);
+
+                while (reader.Read())
+                    if (reader.IsStartElement())
+                    {
+                        if (reader.Name == "map")
+                        {
+                            _width = Convert.ToInt32(reader.GetAttribute("width"));
+                            _height = Convert.ToInt32(reader.GetAttribute("height"));
+                            _tileWidth = Convert.ToInt32(reader.GetAttribute("tilewidth"));
+                            _tileHeight = Convert.ToInt32(reader.GetAttribute("tileheight"));
+                        }
+
+                        if (reader.Name == "layer")
+                        {
+                            // read the layer into layers
+                        }
+                    }
+            }
+            catch (FileNotFoundException)
+            {
+                throw new FileNotFoundException("Path to tmx level is wrong, make sure");
+            }
+            catch (Exception )
+            {
+                //throw new FormatException("Tmx level has wrong format - did you use xml as output");
+            }
+        }
+        #endregion
+    }
+}
---
+++ LevelReader/LevelReaderTestGame.cs
@@ -0,0 +1,101 @@
+using System.Threading.Tasks;
+using System.Xml;
+using LevelReader.GameFrameWork;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LevelReader
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class LevelReaderTestGame : GameHost
+    {
+        GraphicsDeviceManager _graphics;
+        SpriteBatch _spriteBatch;
+
+        private Texture2D _tiles;
+        private TiledMap _level;
+        private bool _levelLoaded = false;
+        private string _levelInfo;
+        public LevelReaderTestGame()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+             _tiles = Content.Load<Texture2D>("tiles_spritesheet");
+          
+            _level = new TiledMap("Levels/levelTest.tmx");
+
+
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+           
+            
+                if (_level.Width == 30)
+                {
+                    GraphicsDevice.Clear(Color.Green);
+                }
+                else
+                {
+                    GraphicsDevice.Clear(Color.Red);
+                }
+          
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
